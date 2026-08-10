85e0cd3da34864e8be56342d159b778e63e932b7
Hancx55 <taylorlongworthh@gmail.com>
2026-08-10 12:51:11 +0100

    Add project files.

---
+++ ModEntry.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using StardewValley.Menus;
+
+using HarmonyLib;
+using System.Security.Cryptography.X509Certificates;
+
+
+namespace trash2cash
+{
+    /// <summary>The mod entry point.</summary>
+    internal sealed class ModEntry : Mod
+    {
+   
+        public override void Entry(IModHelper helper)
+        {
+
+
+            helper.Events.GameLoop.DayStarted += this.OnDayStarted;
+
+            var harmony = new Harmony(this.ModManifest.UniqueID);
+            harmony.PatchAll();
+
+
+
+        }
+
+        private void OnDayStarted(object? sender, DayStartedEventArgs e)
+        {
+            this.Monitor.Log("GOODMORNING!!!", LogLevel.Info);
+        }
+
+        [HarmonyPatch(typeof(Utility), nameof(Utility.trashItem))]
+        public static class TrashItemPatch
+        {
+            
+            public static bool Prefix(Item item)
+            {
+                if (item == null)
+                    return false;
+                Game1.playSound("coin");
+                Game1.player.Money += item.salePrice() * item.Stack;
+                return false;
+            }
+        }
+
+    }
+}
